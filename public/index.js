5d6f4e76d61d026a07d4d2f64227e7a2171a4eec
Divyanshi Agrawal <divyanshiagrawal07072000@gmail.com>
2021-06-30 16:54:21 +0530

    Screen_Share_One-to-one snapshot mute/unmute_others

--- public/index.js
+++ public/index.js
@@ -1,23 +1,22 @@
-// import helpers from './helpers.js';
-
 const socket = io('/');
 const peer = new Peer();
 let myVideoStream;
 let myId;
+let currentPeer;
 var videoGrid = document.getElementById('video-grid')
 var myvideo = document.createElement('video');
-
-
-
 const showChat = document.querySelector("#showChat");
-const backBtn = document.querySelector(".header__back"); 
-
+const backBtn = document.querySelector(".header__back");
+const inviteButton = document.querySelector("#inviteButton");
+const muteButton = document.querySelector("#muteButton");
+const stopVideo = document.querySelector("#stopVideo");
 
 myvideo.muted = true;
 
-const user = prompt("Enter your name") 
+//WHEN THE USER ENTERS THE ROOM
+const user = prompt("Enter your name")
 
-
+//FUNCTIONING OF BACK BUTTON OF CHAT
 backBtn.addEventListener("click", () => {
   document.querySelector(".main__left").style.display = "flex";
   document.querySelector(".main__left").style.flex = "1";
@@ -25,69 +24,75 @@ backBtn.addEventListener("click", () => {
   document.querySelector(".header__back").style.display = "none";
 });
 
+//WHEN THE CHAT ICON IS CLICKED
 showChat.addEventListener("click", () => {
   document.querySelector(".main__right").style.display = "flex";
-  document.querySelector(".main__right").style.flex = "1";
-  document.querySelector(".main__left").style.display = "none";
+  document.querySelector(".main__right").style.flex = "0.3";
+  document.querySelector(".main__left").style.display = "flex";
+  document.querySelector(".main__left").style.flex = "0.7";
   document.querySelector(".header__back").style.display = "block";
 });
 
 
+//WHEN STREAM STARTS
 const peerConnections = {}
 navigator.mediaDevices.getUserMedia({
-  video:true,
-  audio:true
-}).then((stream)=>{
+  video: true,
+  audio: true
+}).then((stream) => {
   myVideoStream = stream;
-  addVideo(myvideo , stream);
-  peer.on('call' , (call)=>{
+  addVideo(myvideo, stream);
+  peer.on('call', (call) => {
     call.answer(stream);
-      const vid = document.createElement('video');
-    call.on('stream' , (userStream) =>{
-      addVideo(vid , userStream);
+    const vid = document.createElement('video');
+    call.on('stream', (userStream) => {
+      addVideo(vid, userStream);
+      currentPeer=call.peerConnection   
     });
-    call.on('error' , (err)=>{
+    call.on('error', (err) => {
       alert(err)
     });
   });
-}).catch(err=>{
-    alert(err.message)
+}).catch(err => {
+  alert(err.message)
 });
-peer.on('open' , (id)=>{
+peer.on('open', (id) => {
   myId = id;
-  socket.emit("newUser" , id , roomID);
+  socket.emit("newUser", id, roomID);
 })
-peer.on('error' , (err)=>{
+peer.on('error', (err) => {
   alert(err.type);
 });
 
 
-socket.on('userJoined' , id=>{
+//WHEN A USER JOINS THE ROOM
+socket.on('userJoined', id => {
   console.log("new user joined")
-  const call  = peer.call(id , myVideoStream);
+  const call = peer.call(id, myVideoStream);
   const vid = document.createElement('video');
-  call.on('error' , (err)=>{
+  call.on('error', (err) => {
     alert(err);
   })
-  call.on('stream' , userStream=>{
-    addVideo(vid , userStream);
+  call.on('stream', userStream => {
+    addVideo(vid, userStream);
+    currentPeer=call.peerConnection    
   })
-  call.on('close' , ()=>{
+  call.on('close', () => {
     vid.remove();
     console.log("user disconect")
   })
   peerConnections[id] = call;
 });
 
-
-socket.on('userDisconnect' , id=>{
-  if(peerConnections[id]){
+//WHEN THE USER DISCONNECTS
+socket.on('userDisconnect', id => {
+  if (peerConnections[id]) {
     peerConnections[id].close();
   }
 });
 
-
-function addVideo(video , stream){
+//ADDING VIDEO OF USER ON THE SCREEN
+function addVideo(video, stream) {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
     video.play();
@@ -95,14 +100,7 @@ function addVideo(video , stream){
   videoGrid.append(video);
 };
 
-
-
-
-
-const inviteButton = document.querySelector("#inviteButton");
-const muteButton = document.querySelector("#muteButton");
-const stopVideo = document.querySelector("#stopVideo");
-
+//WHEN MUTE AUDIO ICON IS CLICKED
 muteButton.addEventListener("click", () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
@@ -118,6 +116,7 @@ muteButton.addEventListener("click", () => {
   }
 });
 
+//WHEN MUTE VIDEO ICON IS CLICKED
 stopVideo.addEventListener("click", () => {
   const enabled = myVideoStream.getVideoTracks()[0].enabled;
   if (enabled) {
@@ -133,6 +132,7 @@ stopVideo.addEventListener("click", () => {
   }
 });
 
+//WHEN INVITE ICON IS CLICKED
 inviteButton.addEventListener("click", (e) => {
   prompt(
     "Copy this link and send it to people you want to meet with",
@@ -140,39 +140,102 @@ inviteButton.addEventListener("click", (e) => {
   );
 });
 
-
-
-
+//VARIABLES USED IN IMPLEMENTING CHAT
 const messageContainer = document.getElementById('chatting')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('chat_message')
 appendMessage('You Joined')
 
-socket.on('chat-message',(data) => {
-  // console.log(data)
-  // appendMessage(`${data.user}: ${data.message}`)
+//WHEN A USER SENDS THE MESSAGE
+socket.on('chat-message', (data) => {
   appendMessage(`Anonymous: ${data.message}`)
 })
-socket.on('User-connected',user => {
+
+//WHEN THE USER CONNECTS
+socket.on('User-connected', user => {
   // console.log(data)
   // appendMessage(`User with id = ${user} connected`)
   appendMessage(`A new User connected`)
 })
-socket.on('userDisconnect',user => {
+
+//WHEN THE USER DISCONNECTS
+socket.on('userDisconnect', user => {
   // console.log(data)
   // appendMessage(`User with id = ${user} disconnected`)
   appendMessage(`One User disconnected`)
 })
+
+
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault()
   const message = messageInput.value
   appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message',message)
+  socket.emit('send-chat-message', message)
   messageInput.value = ''
 })
 
-function appendMessage(message){
+//FUNCTION TO APPEND MESSAGE
+function appendMessage(message) {
   const messageElement = document.createElement('div')
   messageElement.innerText = message
   messageContainer.append(messageElement)
 }
+
+
+//SCREEN SHARING IMPLMENTATION
+// socket.on('share-share', (video) => {
+//   replaceTrack("video")
+// })
+
+const shareScreen = document.querySelector("#screenshare");
+shareScreen.addEventListener('click', (e) => {
+  
+  navigator.mediaDevices.getDisplayMedia({
+    video: {
+      cursor: "always"
+    },audio: {
+      echoCancellation: true,
+      noiseSuppression: true
+    }
+    
+  }).then((stream) =>{
+    let videoTrack = stream.getVideoTracks()[0];
+    videoTrack.onended = function(){
+      stopScreenShare();
+    }
+    let sender = currentPeer.getSenders().find(function(s){
+      // socket.emit("share-screen",videoTrack)
+      return s.track.kind == videoTrack.kind
+    })
+    sender.replaceTrack(videoTrack)
+  }).catch((err) =>{
+    console.log("unable to display media" + err)
+  })
+})
+function stopScreenShare(){
+  let videoTrack = myVideoStream.getVideoTracks()[0];
+  var sender = currentPeer.getSenders().find(function(s){
+    return s.track.kind == videoTrack.kind;
+  })
+  sender.replaceTrack(videoTrack)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { ExpressPeerServer } = require('peer')
 const peer = ExpressPeerServer(server, {
   debug: true
 });
-app.use('/peerjs', peer); //peerServer
+app.use('/peerjs', peer); //PEERSERVER
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.get('/', (req, res) => {
@@ -24,6 +24,7 @@ server.listen(port, () => {
 
 const users = {}
 io.on("connection", (socket) => {
+  //WHEN NEW USER CONNECTS
   socket.on('newUser', (id, room) => {
     console.log('New User')
     users[socket.id] = id
@@ -31,11 +32,26 @@ io.on("connection", (socket) => {
     socket.join(room);
     socket.to(room).broadcast.emit('userJoined', id);
   })
+  //WHEN THERE IS A MESSAGE BY A USER
   socket.emit('chat-message', 'Hellouser')
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
 
   })
+
+ //FOR SCREEN SHARING STARTS
+ socket.on('share-screen',video => {
+  socket.broadcast.emit('share-share', video)
+
+})
+//  socket.on("screen-data", function(data) {
+//   data = JSON.parse(data);
+//   var room = data.room;
+//   var imgStr = data.image;
+//   socket.broadcast.to(room).emit('screen-data', imgStr);
+// })
+
+  //WHEN A USER DISCONNECTS
   socket.on('disconnect', (id, room) => {
     console.log('User disconnected')
     socket.to(room).broadcast.emit('userDisconnect', id);
